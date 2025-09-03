51d9c24f51dc2b356f481233c0557db975c67b02
watchern <watchern@126.com>
2025-09-03 19:24:28 +0800

    修复build:rn时报错Error: spawn EINVAL

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,18 @@
 // metro.config.js
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig: getExpoDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig: getRNDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { getMetroConfig } = require('@tarojs/rn-supporter');
 
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
-
+const { wrapWithReanimatedMetroConfig, } = require('react-native-reanimated/metro-config');
 // 先获取 Taro 的 Metro 配置
 const taroConfig = async () => {
   return await getMetroConfig();
 }
 
 // 获取 Expo 默认配置并与 Taro 配置合并
-const defaultConfig = getDefaultConfig(__dirname);
+const expoDefaultConfig = getExpoDefaultConfig(__dirname);
+// 获取 RN 默认配置并与 Taro 配置合并
+const rnDefaultConfig = getRNDefaultConfig(__dirname);
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -20,10 +20,9 @@ const defaultConfig = getDefaultConfig(__dirname);
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
-const finalConfig = mergeConfig(defaultConfig, taroConfig, config)
 
 module.exports = (async function () {
   return wrapWithReanimatedMetroConfig(
-    mergeConfig(defaultConfig, await getMetroConfig(), config)
+    mergeConfig(expoDefaultConfig, rnDefaultConfig, await getMetroConfig(), config)
   );
 })();
