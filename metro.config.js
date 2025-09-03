18d0d51654636c1b3d6d1af8a3358a1f69f0272a
watchern <watchern@126.com>
2025-09-03 20:06:02 +0800

    修复build:rn时报错Error: spawn EINVAL

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,16 @@
+const { getDefaultConfig: getRNDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 // metro.config.js
 const { getDefaultConfig: getExpoDefaultConfig } = require('@expo/metro-config');
-const { getDefaultConfig: getRNDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 const { getMetroConfig } = require('@tarojs/rn-supporter');
 
-const { wrapWithReanimatedMetroConfig, } = require('react-native-reanimated/metro-config');
 // 先获取 Taro 的 Metro 配置
-const taroConfig = async () => {
-  return await getMetroConfig();
-}
+const taroConfig = getMetroConfig();
 
-// 获取 Expo 默认配置并与 Taro 配置合并
-const expoDefaultConfig = getExpoDefaultConfig(__dirname);
 // 获取 RN 默认配置并与 Taro 配置合并
 const rnDefaultConfig = getRNDefaultConfig(__dirname);
+// 获取 Expo 默认配置并与 Taro 配置合并
+const expoDefaultConfig = getExpoDefaultConfig(__dirname);
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
