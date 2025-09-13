9ecc14c014a5dc514a0dbbe462f541b9810a28ef
watchern <watchern@126.com>
2025-09-13 23:28:21 +0800

    增加tailwindscss恢复css样式导入使用import(RN)

--- metro.config.js
+++ metro.config.js
@@ -18,7 +18,7 @@ const expoDefaultConfig = getExpoDefaultConfig(__dirname);
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = { input: './src/styles/tailwind.scss' };
+const config = { };
 
 const finalConfig = {
   ...expoDefaultConfig,
